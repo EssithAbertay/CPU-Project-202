27049ceef81b10eeef4d6906cd6410ed9fdeb78d
EssithAbertay <2204636@uad.ac.uk>
2024-02-22 02:03:12 +0000

    functioning game of life
    still to implement, number of loops, custom starting, other stuff

--- CMP 202 - CPU project - Ari Thomson - 2204636/CMP 202 - CPU project - Ari Thomson - 2204636/Source.cpp
+++ CMP 202 - CPU project - Ari Thomson - 2204636/CMP 202 - CPU project - Ari Thomson - 2204636/Source.cpp
@@ -4,6 +4,8 @@
 #include <barrier>
 #include <mutex>
 #include <string>
+#include <semaphore>
+#include <atomic>
 
 //The rules of conways game of life, taken from wikipedia https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
 //Any live cell with fewer than two live neighbors dies, as if by underpopulation.
@@ -11,7 +13,6 @@
 //Any live cell with more than three live neighbors dies, as if by overpopulation.
 //Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction
 
-// current error, updating cells before checking reliant cells
 // currently assuming running with 16 threads, will later work on runniing with 8 threads
 
 // ANSI color codes for terminal text coloring
@@ -24,12 +25,20 @@ const char* ANSI_YELLOW = "\033[43m";
 bool cells[10][10]; //create a 10*10 grid but only use the inner 8*8, so that when parsing through data later issues don't arise, the outer layer should always be false/deaad
 
 int numOfThreads = std::thread::hardware_concurrency();
-std::barrier cellUpdateBarrier{ numOfThreads };
+
+std::barrier cellUpdateBarrier{16};
 
 std::mutex textMutex;
+std::mutex gridMutex;
+
+std::counting_semaphore displaySignaller{numOfThreads};
+
+std::atomic<bool> simulationActive;
 
 void setUpCells()
 {
+	simulationActive = true;
+
 	for (int i = 0; i < 10; i++)
 	{
 		for (int j = 0; j < 10; j++)
@@ -38,18 +47,19 @@ void setUpCells()
 		}
 	}
 
-	cells[1][2] = true;
-	cells[2][1] = true;
-	cells[2][2] = true;
-	//cells[4][4] = true;
-	//cells[5][5] = true;
-	//cells[6][6] = true;
-	//cells[7][7] = true;
-	//cells[8][8] = true;
+
+	//X and Y are swapped just now, idk why
+	cells[6][7] = true;
+	cells[7][6] = true;
+	cells[8][6] = true;
+	cells[7][7] = true;
+	cells[8][8] = true;
 }
 
 void displayCells()
 {
+	std::cout << std::endl;
+	std::cout << "\x1B[2J\x1B[H";
 	for (int i = 1; i < 9; i++) //only display the inner area of the cell grid
 	{
 		for (int j = 1; j < 9; j++)
@@ -66,8 +76,8 @@ void displayCells()
 			std::cout << ANSI_RESET;
 		std::cout << std::endl;
 	}
-	std::cout << ANSI_RESET;
-	std::cout << std::endl;
+	std::cout << ANSI_BLUE << "--------";
+	std::cout << ANSI_RESET << std::endl;
 }
 
 bool checkCell(int x, int y)
@@ -81,11 +91,8 @@ void updateCell(bool update, int x, int y)
 }
 
 
-
-
 bool lifeCheck(int x, int y) //check the status of surrounding cells, and then update accordingly
 {
-	//int cellState = 0;
 	int surrounding = 0; //increment for every alive cell surrounding
 	bool currentlyAlive = cells[x][y];
 
@@ -96,42 +103,36 @@ bool lifeCheck(int x, int y) //check the status of surrounding cells, and then u
 			surrounding++;
 		}
 
-		if (cells[(x + -1) + i][y + 1]) //checks row below
+		if (cells[(x -1) + i][y + 1]) //checks row below
 		{
 			surrounding++;
 		}
 	}
 
 	//checks cells to sides
-	if (cells[x-1][y]) //checks row below
+	if (cells[x-1][y]) 
 	{
 		surrounding++;
 	}
 
-	if (cells[x - 1][y]) //checks row below
+	if (cells[x + 1][y]) 
 	{
 		surrounding++;
 	}
 
-	if (currentlyAlive)
+	if (currentlyAlive) //finds what the new value should be
 	{
 		if (surrounding < 2)
 		{
 			return false;
-			//cellState = 0;
-			//updateCell(false, x, y);
 		}
 		else if (surrounding < 4)
 		{
 			return true;
-			//cellState = 1;
-			//updateCell(true, x, y);
 		}
 		else
 		{
 			return false;
-			//cellState = 2;
-		//	updateCell(false, x, y);
 		}
 	}
 	else
@@ -139,33 +140,12 @@ bool lifeCheck(int x, int y) //check the status of surrounding cells, and then u
 		if (surrounding == 3)
 		{
 			return true;
-			//cellState = 3;
-			//updateCell(true, x, y);
+		}
+		else
+		{
+			return false;
 		}
 	}
-
-	//switch (cellState)
-	//{
-	//case 0:
-	//	//updateCell(false, x, y);
-	//	return false;
-	//	break;
-	//case 1:
-	//	//updateCell(true, x, y);
-	//	return true;
-	//	break;
-	//case 2:
-	////	updateCell(false, x, y);
-	//	return false;
-	//	break;
-	//case 3:
-	//	//updateCell(true, x, y);
-	//	return true;
-	//	break;
-	//default:
-	//	break;
-	//}
-
 }
 
 void lifeCheckAllCells()
@@ -189,62 +169,73 @@ void displayText(std::string text)
 
 void updateChunk(int startCellX, int startCellY) //start cell will be the cell in the top left of the chunk
 {
-	bool chunk[2][2];
-	
-	for (int i = 0; i < 2; i++)
+	while (simulationActive)
 	{
-		for (int j = 0; j < 2; j++)
+		bool chunk[2][2];
+
+		for (int y = 0; y < 2; y++)
 		{
-			chunk[i][j] = lifeCheck(startCellX + i, startCellY + j);
+			for (int x = 0; x < 2; x++)
+			{
+				chunk[x][y] = lifeCheck(startCellX + x, startCellY + y);
+			}
 		}
-	}
 
-	
-	displayText(std::string("Chunk " + std::to_string(startCellX) + "," + std::to_string(startCellY) + " completed and waiting"));
+		cellUpdateBarrier.arrive_and_wait();
 
-
-	cellUpdateBarrier.arrive_and_wait();
-
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
+		for (int y = 0; y < 2; y++)
 		{
-			updateCell(chunk[i][j],startCellX + i, startCellY + j);
+			for (int x = 0; x < 2; x++)
+			{
+				updateCell(chunk[x][y], startCellX + x, startCellY + y);
+			}
 		}
-	}
 
-	displayText(std::string("Chunk " + std::to_string(startCellX) + "," + std::to_string(startCellY) + " finished updating"));
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	}
+}
+
+void displayGrid()
+{
+	gridMutex.lock();
+	displayCells();
+	gridMutex.unlock();
 }
 
 int main()
 {
+	//int runs;
+	//std::cout << "Enter number of runs" << std::endl;
+	//std::cin >> runs;
+	setUpCells();
+	displayGrid();
+
 	std::vector<std::thread> chunkThreads;
 
-	//make sure the number of threads being used can divide into 64 cleanly so that the chunks are fine
-	//while (64 % numOfThreads != 0)
-	//{
-	//	numOfThreads--;
-	//}
-	setUpCells();
-	displayCells();
 
-	for (int i = 1; i < 9; i+=2)
+
+	//uses 16 threads to make a 4*4 field, will change later to set it up so that it uses a varying number of cores
+	for (int y = 1; y < 8;y+=2)
 	{
-		for (int j = 1; j < 9; j+=2)
+		for (int x = 1; x< 8; x+=2)
 		{
-			//std::cout << "coords: " << i << ", " << j << std::endl;
-			chunkThreads.push_back(std::thread(updateChunk, i, j));
+			chunkThreads.push_back(std::thread(updateChunk, x, y));
 		}
 	}
 
+	std::thread displayThread([&]() {
+		while (simulationActive) {
+			displayGrid();
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+	});
 
 	for (int i = 0; i < 16; i++)
 	{
 		chunkThreads[i].join();
 	}
 
-
-	displayCells();
+	
 
 	return 0;
 }
