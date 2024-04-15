ad94f003848c5a567956a1ea9186bc7116aba815
Ari Thomson <2204636@uad.ac.uk>
2024-04-16 00:24:37 +0100

    updates

--- CMP 202 - CPU project - Ari Thomson - 2204636/CMP 202 - CPU project - Ari Thomson - 2204636/Source.cpp
+++ CMP 202 - CPU project - Ari Thomson - 2204636/CMP 202 - CPU project - Ari Thomson - 2204636/Source.cpp
@@ -24,16 +24,41 @@ const char* ANSI_YELLOW = "\033[43m";
 
 bool cells[14][14]; //create a 10*10 grid but only use the inner 8*8, so that when parsing through data later issues don't arise, the outer layer should always be false/deaad
 
-int numOfThreads = std::thread::hardware_concurrency();
+int numOfCores = std::thread::hardware_concurrency(); 
+
+int total_dead_cells = 0;
+int total_alive_cells = 0;
 
 std::barrier cellUpdateBarrier{16};
 
+//std::barrier cellUpdateBarrierOne{16};
+//std::barrier cellUpdateBarrierTwo{8};
+//std::barrier cellUpdateBarrierThree{4};
+
 std::mutex textMutex;
 std::mutex gridMutex;
+std::mutex cellUpdateMtx;
+std::mutex aliveCellsMutex;
+std::mutex deadCellsMutex;
 
-std::counting_semaphore displaySignaller{numOfThreads};
+std::condition_variable cv;
 
 std::atomic<bool> simulationActive;
+
+void incrementAliveCells()
+{
+	aliveCellsMutex.lock();
+	total_alive_cells++ ;
+	aliveCellsMutex.unlock();
+}
+
+void incrementDeadCells()
+{
+	deadCellsMutex.lock();
+	total_dead_cells++;
+	deadCellsMutex.unlock();
+}
+
 
 void displayCells(bool includeCoords)
 {
@@ -117,6 +142,7 @@ bool checkCell(int x, int y)
 
 void updateCell(bool update, int x, int y)
 {
+	std::unique_lock<std::mutex> lock(cellUpdateMtx);
 	cells[x][y] = update;
 }
 
@@ -131,7 +157,6 @@ void toggleCell(int x, int y)
 		cells[x][y] = true;
 	}
 }
-
 
 bool lifeCheck(int x, int y) //check the status of surrounding cells, and then update accordingly
 {
@@ -210,30 +235,139 @@ void displayText(std::string text)
 
 void updateChunk(int startCellX, int startCellY) //start cell will be the cell in the top left of the chunk
 {
+	bool chunk[3][3]; 
+	
 	while (simulationActive)
 	{
-		bool chunk[3][3];
-
 		for (int y = 0; y < 3; y++)
 		{
 			for (int x = 0; x < 3; x++)
 			{
 				chunk[x][y] = lifeCheck(startCellX + x, startCellY + y);
+				
+				// update the total number of alive or dead cells throughout the simulation
+				if(chunk[x][y])
+				{
+					incrementAliveCells();
+				}
+				else
+				{
+					incrementDeadCells();
+				}
 			}
 		}
-
+	
 		cellUpdateBarrier.arrive_and_wait();
-
+	
 		for (int y = 0; y < 3; y++)
 		{
 			for (int x = 0; x < 3; x++)
 			{
 				updateCell(chunk[x][y], startCellX + x, startCellY + y);
+
+				
 			}
 		}
-
+		
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
 	}
+
+	
+	//std::vector<bool> values;
+	////int x_size, y_size;
+	////switch (size_case)
+	////{
+	////case(1):
+	////	x_size = 3;
+	////	y_size = 3;
+	////	break;
+	////case(2):
+	////	x_size = 3;
+	////	y_size = 6;
+	////	break;
+	////case(3):
+	////	x_size = 6;
+	////	y_size = 6;
+	////	break;
+	////default:
+	////	break;
+	////}
+	//
+	////for (int y = 0; y < y_size; y++)
+	////{
+	////	for (int x = 0; x < x_size; x++)
+	////	{
+	////		values.push_back(false);
+	////	}
+	////}
+	//
+	//while (simulationActive)
+	//{
+	//	int i = 0;
+	//	for (int y = 0; y < y_size; y++)
+	//	{
+	//		for (int x = 0; x < x_size; x++)
+	//		{
+	//			values[i] = lifeCheck(startCellX + x, startCellY + y);
+	//			i++;
+	//		}
+	//	}
+	//	
+	//	cellUpdateBarrier.arrive_and_wait();
+	//
+	//	//switch (size_case) //this is a very stupid thing, but I'm tired and don't want to think about it anymore
+	//	//{
+	//	//case(1):
+	//	//	cellUpdateBarrierOne.arrive_and_wait();
+	//	//	break;
+	//	//case(2):
+	//	//	cellUpdateBarrierTwo.arrive_and_wait();
+	//	//	break;
+	//	//case(3):
+	//	//	cellUpdateBarrierThree.arrive_and_wait();
+	//	//	break;
+	//	//}
+	//
+	//	i = 0;
+	//
+	//	for (int y = 0; y < y_size; y++)
+	//	{
+	//		for (int x = 0; x < x_size; x++)
+	//		{
+	//			updateCell(values[i], startCellX + x, startCellY + y);
+	//			i++;
+	//		}
+	//	}
+	//
+		//std::vector<bool> values;
+	////int x_size, y_size;
+	////switch (size_case)
+	////{
+	////case(1):
+	////	x_size = 3;
+	////	y_size = 3;
+	////	break;
+	////case(2):
+	////	x_size = 3;
+	////	y_size = 6;
+	////	break;
+	////case(3):
+	////	x_size = 6;
+	////	y_size = 6;
+	////	break;
+	////default:
+	////	break;
+	////}
+//
+	////for (int y = 0; y < y_size; y++)
+	////{
+	////	for (int x = 0; x < x_size; x++)
+	////	{
+	////		values.push_back(false);
+	////	}
+	////}
+
 }
 
 void displayGrid(bool includeCoords)
@@ -243,11 +377,41 @@ void displayGrid(bool includeCoords)
 	gridMutex.unlock();
 }
 
+void updateBoard(int numOfSteps) //start cell will be the cell in the top left of the chunk not using threads
+{
+	bool chunk[12][12];
+	
+	for(int i = 0; i < numOfSteps; i++)
+	{
+		for (int y = 0; y < 12; y++)
+		{
+			for (int x = 0; x < 12; x++)
+			{
+				chunk[x][y] = lifeCheck(x,y);
+			}
+		}
+
+		for (int y = 0; y < 12; y++)
+		{
+			for (int x = 0; x < 12; x++)
+			{
+				updateCell(chunk[x][y],x,y);
+			}
+		}
+		displayGrid(false);
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+		
+	}
+
+}
+
 void setUpCells()
 {
 	bool setupComplete = false;
-	int chosenCell = 0;
+	int chosen = 0;
 	int xC, yC;
+	std::string inputString;
 	for (int i = 0; i < 14; i++)
 	{
 		for (int j = 0; j < 14; j++)
@@ -256,24 +420,129 @@ void setUpCells()
 		}
 	}
 
-	while (!setupComplete)
-	{	
-		displayText("Current state of cell grid");
-		displayGrid(true);
-		displayText("Toggle a cell (1), or continue (2)");
-		std::cin >> chosenCell;
+	displayText("Use a preset cell combination (1) or create your own? (2)");
+	std::cin >> chosen;
 
-		if (chosenCell == 2)
+	switch (chosen)
 		{
-			setupComplete = true;
-		}
-		else
+		case(1):
+			while (!setupComplete)
+			{
+				displayText("Glider (1) \n1-2-3 Oscillator - Dave Buckingham (2) \n1-2-3-4 Oscillator (3)\n");
+				std::cin >> chosen;
+				switch (chosen)
+				{
+				case(1): //glider
+					toggleCell(10, 11);
+					toggleCell(10, 12);
+					toggleCell(11, 10);
+					toggleCell(11, 11);
+					toggleCell(12, 12);
+					break;
+				case(2): //1-2-3 oscillator
+					toggleCell(2, 3);
+					toggleCell(2, 4);
+					toggleCell(3, 4);
+					toggleCell(3, 5);
+					toggleCell(3, 6);
+					toggleCell(4, 2);
+					toggleCell(4, 7);
+					toggleCell(5, 2);
+					toggleCell(5, 3);
+					toggleCell(5, 4);
+					toggleCell(5, 5);
+					toggleCell(5, 7);
+					toggleCell(6, 7);
+					toggleCell(6, 9);
+					toggleCell(6, 10);
+					toggleCell(7, 4);
+					toggleCell(7, 8);
+					toggleCell(7, 10);
+					toggleCell(8, 4);
+					toggleCell(8, 5);
+					toggleCell(8, 6);
+					toggleCell(8, 7);
+					toggleCell(10, 6);
+					toggleCell(10, 7);
+					toggleCell(11, 6);
+					toggleCell(11, 7);
+					break;
+				case(3):
+					toggleCell(2, 5);
+					toggleCell(2, 6);
+					toggleCell(3, 5);
+					toggleCell(4, 6);
+					toggleCell(5, 3);
+					toggleCell(5, 4);
+					toggleCell(5, 5);
+					toggleCell(5, 7);
+					toggleCell(6, 2);
+					toggleCell(6, 7);
+					toggleCell(6, 10);
+					toggleCell(7, 1);
+					toggleCell(7, 3);
+					toggleCell(7, 5);
+					toggleCell(7, 7);
+					toggleCell(7, 9);
+					toggleCell(7, 11);
+					toggleCell(8, 2);
+					toggleCell(8, 7);
+					toggleCell(8, 10);
+					toggleCell(9, 3);
+					toggleCell(9, 4);
+					toggleCell(9, 5);
+					toggleCell(9, 7);
+					toggleCell(10, 6);
+					toggleCell(11, 5);
+					toggleCell(12, 5);
+					toggleCell(12, 6);
+					break;
+				default:
+					break;
+				} //choose a preset
+
+				displayGrid(false);
+				displayText("Ready? (Y/N)");
+				std::cin >> inputString;
+				if (inputString == "Y" || inputString == "y")
+				{
+					setupComplete = true;
+				}
+			}
+			break;
+		case(2):
+			displayGrid(true);	
+			while (!setupComplete)
+			{					
+				displayText("X:");			
+				std::cin >> xC;
+				displayText("Y:");
+				std::cin >> yC;
+				toggleCell(yC, xC);
+				displayGrid(true);
+				displayText("Ready? (Y/N)");
+				std::cin >> inputString;
+				if (inputString == "Y" || inputString == "y")
+				{
+					setupComplete = true;
+				}
+			}
+			break;
+	}
+	
+	//get the starting number of alive and dead cells
+	for (int y = 1; y < 12; y ++)
+	{
+		for (int x = 1; x < 12; x++)
 		{
-			displayText("X:");
-			std::cin >> xC;
-			displayText("Y:");
-			std::cin >> yC;
-			toggleCell(yC, xC);
+			if (cells[x][y])
+			{
+				total_alive_cells++;
+			}
+			else
+			{
+				total_dead_cells++;
+			}
 		}
 	}
 	simulationActive = true;
@@ -282,18 +551,57 @@ void setUpCells()
 int main()
 {
 	int steps;
-	std::cout << "Enter number of steps" << std::endl;
+
+	displayText("Enter number of steps");
 	std::cin >> steps;
 
+	int MaxSteps = steps;
+
 	setUpCells();
-	displayGrid(false);
 
 	std::vector<std::thread> chunkThreads;
 
 	//uses 16 threads to make a 4*4 field, will change later to set it up so that it uses a varying number of threads based on cores
-	for (int y = 1; y < 12;y+=3)
+	//int interval_y, interval_x, size_case; //based on num of cores create more/less chunks
+	//
+	//interval_x = 3;
+	//interval_y = 3;
+	//
+	//
+	//removed, was originally going to have it cores dependent, but ran out of time
+	//if (numOfCores >= 16) //16 cores - 4*4 grid of chunks - each cell is 3*3
+	//{
+	//	interval_x = 3;
+	//	interval_y = 3;
+	//	size_case = 1;
+	//}
+	//else if (numOfCores >= 8) //8 cores - 4*2 grid of chunks - each cell is 3*6
+	//{
+	//	interval_x = 3;
+	//	interval_y = 6;
+	//	size_case = 2;
+	//}
+	//else //anything less than 8 2*2 grid - each cell is 6*6
+	//{
+	//	interval_x = 6;
+	//	interval_y = 6;
+	//	size_case = 3;
+	//}
+	//
+	//
+	//for (int y = 1; y < 12;y+= interval_y)
+	//{
+	//	for (int x = 1; x< 12; x+= interval_x)
+	//	{
+	//		chunkThreads.push_back(std::thread(updateChunk, x, y, size_case));
+	//	}
+	//}
+
+
+	//16 threads for a 4*4 grid
+	for (int y = 1; y < 12; y+= 3)
 	{
-		for (int x = 1; x< 12; x+=3)
+		for (int x = 1; x < 12; x+= 3)
 		{
 			chunkThreads.push_back(std::thread(updateChunk, x, y));
 		}
@@ -313,10 +621,19 @@ int main()
 	
 	displayThread.join();
 
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < chunkThreads.size(); i++)
 	{
 		chunkThreads[i].join();
 	}
+
+
+	std::cout << "Throughout the simulation " << MaxSteps << " steps were performed\nThere were a total of " << total_alive_cells << " alive cells, " << float(total_alive_cells / MaxSteps) << " on average per step\nThere were a total of " << total_dead_cells << " dead cells, " << float(total_dead_cells / MaxSteps) << " on average per step" << std::endl;
+
+
+	//setUpCells();
+	//updateBoard(alsoSteps);
+
+
 
 	return 0;
 }
